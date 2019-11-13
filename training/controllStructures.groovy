ba87914924a0eb9ca64fa9bcef5dc30e56a3f41c
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-11-13 14:53:56 +0100

    add variables and controll structures

---
+++ training/controllStructures.groovy
@@ -0,0 +1,12 @@
+def message = 'Hello'
+def number = 9
+def hugo
+message = null
+
+if (message == null){
+    println ("message was not defined")
+}
+else {
+    println ("message was ${hugo}")
+}
+
---
+++ training/variables.groovy
@@ -0,0 +1,14 @@
+def message = "Hello"
+def number = 42
+def hugo
+println message
+println message + number
+
+message = 9
+println message + number
+
+hugo = message
+println hugo
+message = null
+println message
+
