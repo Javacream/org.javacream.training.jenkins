069cd297c872297d1eb67d507755c417f78701ca
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-07-15 15:56:27 +0200

    add presentation

--- Groovy Basics/src/ToDoCollections.groovy
+++ Groovy Basics/src/ToDoCollections.groovy
@@ -1,4 +1,4 @@
-List names = ["Hugo", "Emil", "Fritz", "Egon", "Anton"]
+List names = ["Hugo", "Emil", "Fritz", "Eduard", "Anton"]
 
 List filteredByE = names.findAll {e -> return e.startsWith("E")}
 filteredByE.each {e -> println(e)}
@@ -8,3 +8,6 @@ sorted.each {e -> println(e)}
 
 List transformed = names.collect {e -> return e.length()}
 transformed.each {e -> println(e)}
+
+
+names.findAll({e -> return e.startsWith("E")}).toSorted({e1, e2 -> return e1.compareTo(e2)}).collect({e -> return e.length()}).each({e -> println(e)})
