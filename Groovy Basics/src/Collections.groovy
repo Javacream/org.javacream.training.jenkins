33d7d2f458f3e5cf5200c8ac09a078f7c5370a15
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-07-15 14:17:40 +0200

    add presentation

--- Groovy Basics/src/Collections.groovy
+++ Groovy Basics/src/Collections.groovy
@@ -4,3 +4,8 @@ def names = ["Hugo", "Emil", "Fritz"]
 println(names[0])
 //println(names[3])
 names[3] = "Franz"
+
+//Iterieren
+for (def i = 0; i < names.size(); i++){
+    println("Element ${i + 1}: ${names[i]}")
+}
