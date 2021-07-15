e3969bba6f0a7a4e5b02ba4f12f2c74fa22f67d0
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-07-15 15:00:00 +0200

    add presentation

--- Groovy Basics/src/Functional.groovy
+++ Groovy Basics/src/Functional.groovy
@@ -1,3 +1,10 @@
+//message = variable, an diese Variable wird zugewiesen ein String-Literal
+def message = "Hello World"
+def number = 9
+def state = true
+def names = ["Hugo", "Emil"]
+def postalCode = [81373: "München", 71234: "Stuttgart"]
+//myFunc ist eine Variable, an diese Variable wird zugewiesen ein Funktions-Literal
 def myFunc = {x, y -> return x + y}
 println myFunc(1, 2)
 
