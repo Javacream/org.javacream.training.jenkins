1e4908d53b3461dc1b1b5f9a08a55e62dc7da47b
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-09-20 14:27:13 +0200

    call by reference

--- Groovy Basics/src/Books.groovy
+++ Groovy Basics/src/Books.groovy
@@ -1,5 +1,5 @@
 class Book{
-    String title
+    final String title
     String isbn
     double price
     int pages
@@ -9,6 +9,20 @@ class Book{
         this.isbn = isbn
         this.price = price
         this.pages = pages
+    }
+}
+
+class BooksController{
+    Map<String, Book> books = new HashMap();
+    Random r = new Random()
+    String create(String title){
+        String isbn = "ISBN: " + r.nextInt()
+        Book newBook = new Book(isbn, title, 19.99, 200)
+        books.put(isbn, newBook)
+        return isbn
+    }
+    Book findBookByIsbn(String isbn){
+        return books.get(isbn)
     }
 
 
