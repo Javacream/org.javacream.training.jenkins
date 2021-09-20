bb05aac7faf00787933fbc930820a7a4d4affa8d
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-09-20 13:42:50 +0200

    call by reference

---
+++ Groovy Basics/src/Books.groovy
@@ -0,0 +1,15 @@
+class Book{
+    String title
+    String isbn
+    double price
+    int pages
+
+    Book(String isbn, String title, double price, int pages) {
+        this.title = title
+        this.isbn = isbn
+        this.price = price
+        this.pages = pages
+    }
+
+
+}
